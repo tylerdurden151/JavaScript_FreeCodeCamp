1e080119c9618122a30d5d3f99604ffb36b6b401
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-17 17:18:00 -0700

    Completed project

--- 02-Build_a_Reusable_Footer/src/components/Footer.jsx
+++ 02-Build_a_Reusable_Footer/src/components/Footer.jsx
@@ -5,9 +5,15 @@ const Footer = () => {
             <ul className="footer-links">
                 <li><a href="/about">About Us</a></li>
                 <li><a href="/contact">Contact</a></li>
-                <li><a href="/privacy">Privacy Policy</a></li>
             </ul>
-
+            <ul className="social-links">
+                <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            </ul>
+            <ul className="legal-links">
+                <li><a href="/privacy">Privacy Policy</a></li>
+                <li><a href="/terms">Terms of Service</a></li>
+            </ul>
             <p>&copy; 2023 My Company. All rights reserved.</p>
         </div>
     </footer>
