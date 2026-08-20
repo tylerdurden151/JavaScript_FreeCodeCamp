c4e5c3d90d1b3bc53a34d7db45062a1c794fac77
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-20 16:19:41 -0700

    Assignment Complete

---
+++ 03-Build_a_Reusable_Profile_Card_Component/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card({ name, title, bio }) {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <p className="card-title">{title}</p>
+      <p>{bio}</p>
+    </div>
+  )
+}
+
+export { Card }
+export default Card
